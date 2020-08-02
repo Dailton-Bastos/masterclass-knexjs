563a16b962419812ccad3129a286a25ad329772d
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-02 19:04:45 -0300

    feat: update user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -23,4 +23,18 @@ module.exports = {
       return next(error);
     }
   },
+
+  async update(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const { username } = req.body;
+
+      await knex('users').update({ username }).where({ id });
+
+      return res.send();
+    } catch (error) {
+      return next(error);
+    }
+  },
 };
