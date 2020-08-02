beac1775b22756f2b630362fd761fdac6dc793d9
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-02 18:52:16 -0300

    feat: get all error messages

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const routes = require('./routes');
 
 const server = express();
 
+server.use(express.json());
 server.use(routes);
+
+// catch all
+
+server.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+
+  return next();
+});
 
 server.listen(3333, () => console.log('Server on'));
